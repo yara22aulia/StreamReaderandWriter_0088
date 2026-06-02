cd5ae44dec49d6d1fcbb2fe9d28790ab3b70767d
yara22aulia <yara22aulia@gmail.com>
2026-06-02 16:45:46 +0700

    menambahkan penanganan exception integer dan default

--- ManualException.cpp
+++ ManualException.cpp
@@ -12,12 +12,12 @@ int main()
     }
 
     //kondisi jika throw yang bertipe data integer
-    catch (int a) {
+    catch (int a)
+    {
         cout << "Pengecualian akan di eksekusi" << endl;
     }
     //kondisi jika throw bertipe data selain integer
-    catch (...)
-    {
+    catch (...) {
         cout << "default pengecualian diekseskusi" << endl;
     }
 }
