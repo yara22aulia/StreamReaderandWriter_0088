0fa0d4db26c60219e34c2e50932cf53be956c4aa
yara22aulia <yara22aulia@gmail.com>
2026-06-02 16:44:47 +0700

    menambahkan blok try

--- ManualException.cpp
+++ ManualException.cpp
@@ -8,11 +8,11 @@ int main()
     {
         cout << "Selamat belajar di prodi TI UMY" << endl;
         //throw 0.5; //melemparkan sebuah integer maka
-        cout << "Pernyataan tidak akan ditampilkan" << endl;}
+        cout << "Pernyataan tidak akan ditampilkan" << endl;
+    }
 
     //kondisi jika throw yang bertipe data integer
-    catch (int a)
-    {
+    catch (int a) {
         cout << "Pengecualian akan di eksekusi" << endl;
     }
     //kondisi jika throw bertipe data selain integer
