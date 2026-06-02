0915806b0c1e4e727f0ef2db88d902bea63b699d
yara22aulia <yara22aulia@gmail.com>
2026-06-02 17:13:44 +0700

    Input nama file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -10,14 +10,14 @@ int main()
     string baris, NamaFile;
     
     //membuat input untuk menangkap nama file
-    cout << 
-    "Masukkan nama file : ";
+    cout << "Masukkan nama file : ";
     cin >> NamaFile;
     
     //membuat file dalam mode menulis
     ofstream outfile;
     //menunjuk ke sebuah nama file dengan ekstensi txt
-    outfile.open(NamaFile + ".txt", ios::out);
+    outfile.open
+    (NamaFile + ".txt", ios::out);
     
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
     
