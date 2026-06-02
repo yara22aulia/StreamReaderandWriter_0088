46c74d4f28a5fe21d944b5df24dd1178b1fe640c
yara22aulia <yara22aulia@gmail.com>
2026-06-02 16:35:36 +0700

    menambahkan penanganan exception

--- ExceptionStandar.cpp
+++ ExceptionStandar.cpp
@@ -13,12 +13,11 @@ int main()
         //deklarasi array data dengan panjang index 3
         array <int, 3> data = { 1, 2, 3 };
         //memanggil array element ke-5 yang berarti akan terjadi error
-        cout << data.at(5) << endl;}
-        
+        cout << data.at(5) << endl;
+    }
+
     //kondisi jika terjadi error didalam try
-    catch (exception& e)
-    {
-        
+    catch (exception& e) { 
         cout << e.what() << endl;
     }
     cout << "Baris program terakhir" << endl; //penanda 2
