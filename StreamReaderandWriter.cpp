91bc1e486aacf3c969b012bf7e5f4b1f4023ea49
yara22aulia <yara22aulia@gmail.com>
2026-06-02 17:05:41 +0700

    Menangani error saat file gagal dibuka

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -40,11 +40,11 @@ int main()
     
     cout << endl << ">= Membuka dan membaca file " << endl;
     //kondisi jika file ada
-    if (infile.is_open()
-)
+    if (infile.is_open())
     {
         //perulangan untuk memunculkan setiap baris
-        while (getline(infile, baris)) {
+        while (getline(infile, baris)) 
+        {
             cout << baris << endl;
         }
         //menutup file jika telah selesai
